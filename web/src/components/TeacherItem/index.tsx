9df6cb4a30baf024e9b2e327456263b78be56db6
Andre Coelho <andre.coelho@axur.com>
2020-08-05 15:32:53 -0300

    feat: web completed

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface Props {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<Props> = ({ teacher }) => {
+  const handleCreateNewConnection = useCallback(() => {
+    api.post("connections", { user_id: teacher.id });
+  }, []);
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/41406021?s=460&u=8f4d31da309dff79dc943d2290164ac62eebfaf9&v=4"
-          alt="André Coelho"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>André Coelho</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
-          Preço por hora <strong>R$ 120,00</strong>
+          Preço por hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={handleCreateNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Entrar em contato" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- web/src/pages/TacherList/index.tsx
+++ web/src/pages/TacherList/index.tsx
@@ -1,34 +1,117 @@
-import React from "react";
+import React, { useState, useCallback, FormEvent } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import { Teacher } from "../../components/TeacherItem";
 
 import "./styles.css";
+import api from "../../services/api";
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
+
+  const handleSearchTeachers = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+
+      const { data } = await api.get("classes", {
+        params: {
+          subject,
+          week_day,
+          time,
+        },
+      });
+
+      setTeachers(data);
+    },
+    [subject, week_day, time]
+  );
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponiveis">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+        <form id="search-teachers" onSubmit={handleSearchTeachers}>
+          <Select
+            name="subject"
+            label="Matéria"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            options={[
+              {
+                value: "Artes",
+                label: "Artes",
+              },
+              {
+                value: "Biologia",
+                label: "Biologia",
+              },
+              {
+                value: "Educação física",
+                label: "Educação física",
+              },
+              {
+                value: "Matemática",
+                label: "Matemática",
+              },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            value={week_day}
+            onChange={(e) => setWeekDay(e.target.value)}
+            options={[
+              {
+                value: "0",
+                label: "Domingo",
+              },
+              {
+                value: "1",
+                label: "Segunda-feira",
+              },
+              {
+                value: "2",
+                label: "Terça-feira",
+              },
+              {
+                value: "3",
+                label: "Quarta-feira",
+              },
+              {
+                value: "4",
+                label: "Quinta-feita",
+              },
+              {
+                value: "5",
+                label: "Sexta-feira",
+              },
+              {
+                value: "6",
+                label: "Sabado",
+              },
+            ]}
+          />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   );
